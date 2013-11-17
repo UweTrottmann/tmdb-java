4c91252ba3fa1a5481d37255cde4eb2ed299010e
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-11-17 10:30:16 +0100

    Add debug flag to base test case, by default false.

--- src/test/java/com/uwetrottmann/tmdb/BaseTestCase.java
+++ src/test/java/com/uwetrottmann/tmdb/BaseTestCase.java
@@ -3,16 +3,20 @@ package com.uwetrottmann.tmdb;
 import junit.framework.TestCase;
 
 public abstract class BaseTestCase extends TestCase {
-	protected static final String API_KEY = "<FILL-ME-IN>";
-	
-	private final Tmdb manager = new Tmdb();
-	
-	@Override
-	public void setUp() {
-		manager.setApiKey(API_KEY);
-	}
-	
-	protected final Tmdb getManager() {
-		return manager;
-	}
+
+    protected static final String API_KEY = "<FILL-ME-IN>";
+
+    private static final boolean DEBUG = false;
+
+    private final Tmdb manager = new Tmdb();
+
+    @Override
+    public void setUp() {
+        manager.setApiKey(API_KEY);
+        manager.setIsDebug(DEBUG);
+    }
+
+    protected final Tmdb getManager() {
+        return manager;
+    }
 }
