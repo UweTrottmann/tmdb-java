76f4e75abbf3c79b5d44dbe8c551bd14e1c75f33
miguel <miguelj_5@hotmail.com>
2015-02-21 12:47:31 +0000

    Update Person Service

--- src/test/java/com/uwetrottmann/tmdb/services/PersonServiceTest.java
+++ src/test/java/com/uwetrottmann/tmdb/services/PersonServiceTest.java
@@ -1,10 +1,16 @@
 package com.uwetrottmann.tmdb.services;
 
 import com.uwetrottmann.tmdb.BaseTestCase;
+import com.uwetrottmann.tmdb.entities.Image;
+import com.uwetrottmann.tmdb.entities.Media;
 import com.uwetrottmann.tmdb.entities.Person;
 import com.uwetrottmann.tmdb.entities.PersonCastCredit;
 import com.uwetrottmann.tmdb.entities.PersonCredits;
 import com.uwetrottmann.tmdb.entities.PersonCrewCredit;
+import com.uwetrottmann.tmdb.entities.PersonIds;
+import com.uwetrottmann.tmdb.entities.PersonImages;
+import com.uwetrottmann.tmdb.entities.PersonResultsPage;
+
 import org.junit.Test;
 
 import java.text.ParseException;
@@ -64,6 +70,62 @@ public class PersonServiceTest extends BaseTestCase {
         assertThat(credits.id).isEqualTo(287);
         assertCastCredits(credits, true);
         assertCrewCredits(credits, true);
+    }
+    
+    @Test
+    public void test_external_ids() {
+        PersonIds ids = getManager().personService().externalIds(287);
+        assertThat(ids.id).isEqualTo(287);
+        assertEquals("Person IMDB ID was null.", "nm0000093", ids.imdb_id);
+        assertEquals("Person FREEBASE MID was null.", "/m/0c6qh", ids.freebase_mid);
+        assertEquals("Person FREEBASE ID was null.", "/en/brad_pitt", ids.freebase_id);
+        assertThat(ids.tvrage_id).isEqualTo(59436);
+    }
+    
+    @Test
+    public void test_images() {
+        PersonImages images = getManager().personService().images(287);
+        assertThat(images.id).isEqualTo(287);
+        
+        for (Image image : images.profiles) {
+            assertThat(image.file_path).isNotEmpty();
+            assertThat(image.width).isNotNull();
+            assertThat(image.height).isNotNull();
+            assertThat(image.aspect_ratio).isGreaterThan(0);
+        }
+    }
+    
+    @Test
+    public void test_popular() {
+        PersonResultsPage popular = getManager().personService().popular();
+        
+        assertThat(popular.results.get(0).id).isNotNull();
+        assertThat(popular.results.get(0).name).isNotNull();
+        assertThat(popular.results.get(0).popularity).isNotNull();
+        assertThat(popular.results.get(0).profile_path).isNotNull();
+        assertThat(popular.results.get(0).adult).isNotNull();
+        
+        for (Media media : popular.results.get(0).known_for) {
+            assertThat(media.adult).isNotNull();
+            assertThat(media.backdrop_path).isNotNull();
+            assertThat(media.id).isNotNull();
+            assertThat(media.original_title).isNotNull();
+            assertThat(media.release_date).isNotNull();
+            assertThat(media.poster_path).isNotNull();
+            assertThat(media.popularity).isNotNull().isGreaterThan(0);
+            assertThat(media.title).isNotNull();
+            assertThat(media.vote_average).isNotNull().isGreaterThan(0);
+            assertThat(media.vote_count).isNotNull().isGreaterThan(0);
+            assertThat(media.media_type).isNotNull();
+        }
+    }
+    
+    @Test
+    public void test_latest() throws ParseException {
+        Person person = getManager().personService().latest();
+        assertNotNull("Result was null.", person);
+        assertThat(person.name).isNotNull();
+        assertThat(person.id).isNotNull();
     }
 
     private void assertCastCredits(PersonCredits credits, boolean hasMediaType) {
