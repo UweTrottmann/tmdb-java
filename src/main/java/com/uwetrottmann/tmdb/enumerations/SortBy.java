cd3bafa7f1ee63064b3490224c15306a4896ac6c
miguel <miguelj_5@hotmail.com>
2015-02-21 18:06:25 +0000

    Add Discover service

---
+++ src/main/java/com/uwetrottmann/tmdb/enumerations/SortBy.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2015 Miguel Teixeira
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.uwetrottmann.tmdb.enumerations;
+
+public enum SortBy {
+
+    POPULARITY_ASC("popularity.asc"),
+    POPULARITY_DESC("popularity.desc"),
+    RELEASE_DATE_ASC("release_date.asc"),
+    RELEASE_DATE_DESC("release_date.desc"),
+    REVENUE_ASC("revenue.asc"),
+    REVENUE_DESC("revenue.desc"),
+    PRIMARY_RELEASE_DATE_ASC("primary_release_date.asc"),
+    PRIMARY_RELEASE_DATE_DESC("primary_release_date.desc"),
+    ORIGINAL_TITLE_ASC("original_title.asc"),
+    ORIGINAL_TITLE_DESC("original_title.desc"),
+    VOTE_AVERAGE_ASC("vote_average.asc"),
+    VOTE_AVERAGE_DESC("vote_average.desc"),
+    VOTE_COUNT_ASC("vote_count.asc"),
+    VOTE_COUNT_DESC("vote_count.desc");
+
+    private final String value;
+
+    private SortBy(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
