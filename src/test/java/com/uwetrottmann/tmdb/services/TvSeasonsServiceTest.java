7bf3ab78fd933f770fdd7ab201be5ad16d90faf1
miguel <miguelj_5@hotmail.com>
2015-02-21 15:56:36 +0000

    Update Tv seasons Service

--- src/test/java/com/uwetrottmann/tmdb/services/TvSeasonsServiceTest.java
+++ src/test/java/com/uwetrottmann/tmdb/services/TvSeasonsServiceTest.java
@@ -16,10 +16,21 @@
 
 package com.uwetrottmann.tmdb.services;
 
+import java.util.List;
+
 import com.uwetrottmann.tmdb.BaseTestCase;
 import com.uwetrottmann.tmdb.TestData;
+import com.uwetrottmann.tmdb.entities.CastMember;
+import com.uwetrottmann.tmdb.entities.Credits;
+import com.uwetrottmann.tmdb.entities.CrewMember;
+import com.uwetrottmann.tmdb.entities.ExternalIds;
+import com.uwetrottmann.tmdb.entities.Image;
 import com.uwetrottmann.tmdb.entities.TvEpisode;
+import com.uwetrottmann.tmdb.entities.TvImages;
 import com.uwetrottmann.tmdb.entities.TvSeason;
+import com.uwetrottmann.tmdb.entities.Videos;
+import com.uwetrottmann.tmdb.entities.Videos.Video;
+
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,12 +40,99 @@ public class TvSeasonsServiceTest extends BaseTestCase {
     @Test
     public void test_season() {
         TvSeason season = getManager().tvSeasonsService().season(TestData.TVSHOW_ID, 1, null);
-        assertThat(season.season_number).isEqualTo(1);
         assertThat(season.air_date).isNotNull();
+        assertThat(season.name).isEqualTo("Season 1");
+        assertThat(season.overview).isNotNull();
+        assertThat(season.id).isNotNull();
+        assertThat(season.poster_path).isNotEmpty();
+        assertThat(season.season_number).isEqualTo(1);
         assertThat(season.episodes).isNotEmpty();
+        
         for (TvEpisode episode : season.episodes) {
-            assertThat(episode.season_number).isEqualTo(1);
+            assertCrewCredits(episode.crew);
+            assertCastCredits(episode.guest_stars);
+            assertThat(episode.air_date).isNotNull();
             assertThat(episode.episode_number).isPositive();
+            assertThat(episode.name).isNotNull();
+            assertThat(episode.overview).isNotNull();
+            assertThat(episode.id).isNotNull();
+            assertThat(episode.season_number).isEqualTo(1);
+            assertThat(episode.still_path).isNotNull();
+            assertThat(episode.vote_average).isGreaterThanOrEqualTo(0);
+            assertThat(episode.vote_count).isGreaterThanOrEqualTo(0);
+        }
+    }
+    
+    @Test
+    public void test_credits() {
+        Credits credits = getManager().tvSeasonsService().credits(TestData.TVSHOW_ID, 1);
+        assertThat(credits.id).isNotNull();
+        assertCrewCredits(credits.crew);
+        assertCastCredits(credits.cast);
+    }
+    
+    @Test
+    public void test_externalIds() {
+        ExternalIds ids = getManager().tvSeasonsService().externalIds(TestData.TVSHOW_ID, 1, null);
+        assertThat(ids.id).isNotNull();
+        assertThat(ids.freebase_id).isNotNull();
+        assertThat(ids.freebase_mid).isNotNull();
+        assertThat(ids.tvdb_id).isNotNull();
+    }
+    
+    @Test
+    public void test_images() {
+        TvImages images = getManager().tvSeasonsService().images(TestData.TVSHOW_ID, 1, null);
+        assertThat(images.id).isNotNull();
+        
+        for (Image image : images.posters) {
+            assertThat(image.file_path).isNotEmpty();
+            assertThat(image.width).isNotNull();
+            assertThat(image.height).isNotNull();
+            assertThat(image.aspect_ratio).isGreaterThan(0);
+            assertThat(image.vote_average).isGreaterThanOrEqualTo(0);
+            assertThat(image.vote_count).isGreaterThanOrEqualTo(0);
+        }
+    }
+    
+    @Test
+    public void test_videos() {
+        Videos videos = getManager().tvSeasonsService().videos(TestData.TVSHOW_ID, 1, null);
+        assertThat(videos.id).isNotNull();
+        
+        for (Video video : videos.results) {
+            assertThat(video).isNotNull();
+            assertThat(video.id).isNotNull();
+            assertThat(video.iso_639_1).isNotNull();
+            assertThat(video.key).isNotNull();
+            assertThat(video.name).isNotNull();
+            assertThat(video.site).isNotNull();
+            assertThat(video.size).isNotNull();
+            assertThat(video.type).isNotNull();
+        }
+    }
+    
+    private void assertCrewCredits(List<CrewMember> crew) {
+        assertThat(crew).isNotEmpty();
+        
+        for (CrewMember member : crew) {
+            assertThat(member.id).isNotNull();
+            assertThat(member.credit_id).isNotNull();
+            assertThat(member.name).isNotNull();
+            assertThat(member.department).isNotNull();
+            assertThat(member.job).isNotNull();
+        }
+    }
+    
+    private void assertCastCredits(List<CastMember> cast) {
+        assertThat(cast).isNotEmpty();
+        
+        for (CastMember member : cast) {
+            assertThat(member.id).isNotNull();
+            assertThat(member.credit_id).isNotNull();
+            assertThat(member.name).isNotNull();
+            assertThat(member.character).isNotNull();
+            assertThat(member.order).isNotNull();
         }
     }
 
