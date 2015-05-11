b07f1824ab22a6e618e2e0c03f867cac026c3bd2
Uwe Trottmann <uwe.trottmann@gmail.com>
2015-05-11 08:28:41 +0200

    Add origin_country to TV show search results.
    - https://github.com/UweTrottmann/tmdb-java/issues/19

--- src/main/java/com/uwetrottmann/tmdb/entities/TvShow.java
+++ src/main/java/com/uwetrottmann/tmdb/entities/TvShow.java
@@ -1,12 +1,14 @@
 package com.uwetrottmann.tmdb.entities;
 
 import java.util.Date;
+import java.util.List;
 
 public class TvShow {
 
     public Integer id;
     public String original_name;
     public String name;
+    public List<String> origin_country;
     public Date first_air_date;
     public String backdrop_path;
     public String poster_path;
