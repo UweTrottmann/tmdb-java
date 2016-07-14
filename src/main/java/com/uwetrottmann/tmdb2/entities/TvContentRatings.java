7b02d18802d1dcb883007044c8ed1853e5cb0d86
Uwe Trottmann <uwe+git@uwetrottmann.com>
2016-07-14 07:44:41 +0200

    Remove unused import, uses spaces for indentation.

--- src/main/java/com/uwetrottmann/tmdb2/entities/TvContentRatings.java
+++ src/main/java/com/uwetrottmann/tmdb2/entities/TvContentRatings.java
@@ -3,6 +3,6 @@ package com.uwetrottmann.tmdb2.entities;
 import java.util.List;
 
 public class TvContentRatings {
-	public List<ContentRating> results;
-	public Integer id;
+    public List<ContentRating> results;
+    public Integer id;
 }
