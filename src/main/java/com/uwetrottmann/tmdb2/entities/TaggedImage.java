5b3f31bfdbeaeaeea45817fe3ecedd4eeaff5b4c
Nikolas Mavropoylos <nikmaurop@gmail.com>
2017-09-01 12:02:01 +0300

    Fixed minor issues.

--- src/main/java/com/uwetrottmann/tmdb2/entities/TaggedImage.java
+++ src/main/java/com/uwetrottmann/tmdb2/entities/TaggedImage.java
@@ -4,10 +4,7 @@ public class TaggedImage extends Image {
 
     public String id;
     public String media_type;
-    public String title;
-    public String overview;
     public TmdbDate release_date;
     public Media media;
-
 
 }
