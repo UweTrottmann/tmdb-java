4726a08bbba0b9316de0b219a34d40f2ae4f6637
Uwe Trottmann <uwe+git@uwetrottmann.com>
2018-08-08 11:55:28 +0200

    Remove BaseExternalIds, each TV entity gets their own class.
    - Simplify tests.

--- src/main/java/com/uwetrottmann/tmdb2/entities/PersonExternalIds.java
+++ src/main/java/com/uwetrottmann/tmdb2/entities/PersonExternalIds.java
@@ -7,8 +7,8 @@ public class PersonExternalIds {
     public String facebook_id;
     public String freebase_id;
     public String freebase_mid;
-    public String instagram_id;
     public Integer tvrage_id;
+    public String instagram_id;
     public String twitter_id;
 
 }
--- src/main/java/com/uwetrottmann/tmdb2/entities/TvExternalIds.java
+++ src/main/java/com/uwetrottmann/tmdb2/entities/TvExternalIds.java
@@ -1,8 +1,16 @@
 package com.uwetrottmann.tmdb2.entities;
 
-public class TvExternalIds extends BaseExternalIds {
+public class TvExternalIds {
 
+    public Integer id;
     public String imdb_id;
     public Integer tvdb_id;
+    public String facebook_id;
+    public String freebase_id;
+    public String freebase_mid;
+    public String instagram_id;
+    public Integer tvrage_id;
+    public String twitter_id;
+
 
 }
