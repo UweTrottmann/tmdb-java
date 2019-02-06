61a3ced0bb8653b603d47ec4639ec0e8fe24f401
Uwe Trottmann <uwe+git@uwetrottmann.com>
2019-02-06 18:09:35 +0100

    Add EverythingIsNullable, mark all entities with it.

---
+++ src/main/java/com/uwetrottmann/tmdb2/internal/EverythingIsNullable.java
@@ -0,0 +1,24 @@
+package com.uwetrottmann.tmdb2.internal;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import javax.annotation.Nullable;
+import javax.annotation.meta.TypeQualifierDefault;
+
+/**
+ * Extends {@code ParametersAreNullableByDefault} to also apply to method results and fields.
+ *
+ * @see javax.annotation.ParametersAreNullableByDefault
+ */
+@Documented
+@Nullable
+@TypeQualifierDefault({
+        ElementType.FIELD,
+        ElementType.METHOD,
+        ElementType.PARAMETER
+})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EverythingIsNullable {
+}
