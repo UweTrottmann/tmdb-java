1762d8bca0a3f7f5cd63e16fbac146cb020ca78d
Myron <myron0815@gmx.net>
2019-03-07 21:26:38 +0100

    Translations object has been expanded with country, overview, etc...
    see https://api.themoviedb.org/3/movie/9929?api_key=XXXX&language=de&append_to_response=translations
    for an example request

--- src/main/java/com/uwetrottmann/tmdb2/entities/Translations.java
+++ src/main/java/com/uwetrottmann/tmdb2/entities/Translations.java
@@ -6,10 +6,18 @@ public class Translations {
 
     public static class Translation {
 
+        public String iso_3166_1;
         public String iso_639_1;
         public String name;
         public String english_name;
+        public Data data;
 
+        public class Data {
+
+            public String title;
+            public String overview;
+            public String homepage;
+        }
     }
 
     public Integer id;
