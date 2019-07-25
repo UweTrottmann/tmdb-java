778713d6515685a2421af3b42c1d5f960c1f05b2
Uwe Trottmann <uwe+git@uwetrottmann.com>
2019-07-25 13:06:59 +0200

    ReleaseDate: make types actual constants.

--- src/main/java/com/uwetrottmann/tmdb2/entities/ReleaseDate.java
+++ src/main/java/com/uwetrottmann/tmdb2/entities/ReleaseDate.java
@@ -4,12 +4,12 @@ import java.util.Date;
 
 public class ReleaseDate {
 
-    public static Integer TYPE_PREMIERE = 1;
-    public static Integer TYPE_THEATRICAL_LIMITED = 2;
-    public static Integer TYPE_THEATRICAL = 3;
-    public static Integer TYPE_DIGITAL = 4;
-    public static Integer TYPE_PHYSICAL = 5;
-    public static Integer TYPE_TV = 6;
+    public static final int TYPE_PREMIERE = 1;
+    public static final int TYPE_THEATRICAL_LIMITED = 2;
+    public static final int TYPE_THEATRICAL = 3;
+    public static final int TYPE_DIGITAL = 4;
+    public static final int TYPE_PHYSICAL = 5;
+    public static final int TYPE_TV = 6;
 
     public String certification;
     public String iso_639_1;
