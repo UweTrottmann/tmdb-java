5fded393c621543ce9c0f6e1ed5a8985db18d4ae
Chris Banes <chrisbanes@google.com>
2019-11-27 12:38:35 +1100

    Add tests for new network fields

--- src/test/java/com/uwetrottmann/tmdb2/assertions/NetworkAssertions.java
+++ src/test/java/com/uwetrottmann/tmdb2/assertions/NetworkAssertions.java
@@ -6,14 +6,20 @@ import static com.uwetrottmann.tmdb2.TestData.testNetwork;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NetworkAssertions {
-    public static void assertNetwork(Network network) {
+    public static void assertNetwork(Network network, boolean fromTvSummary) {
         assertThat(network).isNotNull();
         assertThat(network.name).isNotNull();
         assertThat(network.id).isNotNull();
+        assertThat(network.origin_country).isNotNull();
+
+        if (fromTvSummary) {
+            // logo_path is only present from a tv summary response
+            assertThat(network.logo_path).isNotNull();
+        }
     }
 
     public static void assertNetworkDataIntegrity(Network network) {
-        assertNetwork(network);
+        assertNetwork(network, false);
         assertThat(network.name).isEqualTo(testNetwork.name);
         assertThat(network.id).isEqualTo(testNetwork.id);
     }
