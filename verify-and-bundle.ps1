94c6a4ef5f11cc9890b00ab644691a8cc524588d
Uwe Trottmann <uwe+git@uwetrottmann.com>
2022-09-01 12:49:33 +0200

    Drop unused bundle script.

--- verify-and-bundle.ps1
+++
@@ -1,9 +0,0 @@
-﻿Write-Host "mvn clean verify -DskipTests=true -P sonatype-oss-release"
-mvn clean verify -DskipTests=true -P sonatype-oss-release
-
-Write-Host "Packaging into bundle.jar..."
-cd ./target
-jar -cvf bundle.jar *.pom *.jar *.asc
-Write-Host "Packaging into bundle.jar...DONE"
-
-Read-Host "Press any key to exit..."
